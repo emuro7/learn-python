f52d3cda9960ce277f4129636311907e362cffdb
Enrique Muro <enrique7989@gmail.com>
2018-10-29 20:36:48 -0500

    Practiced command line program with inputs

---
+++ madlib.py
@@ -0,0 +1,13 @@
+#Store user input in variable called name
+name = input("What is your name?: ")
+#Print to console user input
+print(name)
+#Print variable type
+print(type(name))
+
+movie = input("What is your favorite movie?: ")
+print(movie)
+print(type(movie))
+print("\nYour name is " , name , "\nYou tell John, ")
+print("He tells you, " , movie , " sucks.")
+wyd = input("Wyd?: ")
